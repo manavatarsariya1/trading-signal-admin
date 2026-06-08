02a6f2324cb6d66a5be713fbcda6ff42f659380b
manavatarsariya1 <manavtarsariya@logicgoinfotech.in>
2026-06-08 12:49:27 +0530

    chnaged

--- server/app.ts
+++ server/app.ts
@@ -7,11 +7,15 @@ import { env } from './config/env.js'
 import { errorHandler } from './middlewares/errorHandler.js'
 import { notFound } from './middlewares/notFound.js'
 import apiRoutes from './routes/index.js'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
 
 const app = express()
 
 app.use(helmet())
-app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }))
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }))
 app.use(cookieParser())
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true }))
